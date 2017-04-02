9e3c64eb1285e0b8a452434bb9b1ba146a71dee3
hnhuyit <hnhuyit@gmail.c>
2017-04-02 20:48:44 +0700

    create directive

--- web/app/modules/admin-core/view/client/js/status.client.directive.js
+++ web/app/modules/admin-core/view/client/js/status.client.directive.js
@@ -1,9 +1,9 @@
 angular.module('core')
 	.directive('status', function () {
 		return {
-			restrict: 'EA', //E = element, A = attribute, C = class, M = comment
+			restrict: 'EAC', //E = element, A = attribute, C = class, M = comment
 			link: function ($scope, element, attrs) {
-				var tag =  '<span class="label label-warning">unpublish</span>';
+				var tag =  '<span class="label label-danger">unpublish</span>';
 				
 				if(attrs.status==1){
 					tag =  '<span class="label label-success">publish</span>';
